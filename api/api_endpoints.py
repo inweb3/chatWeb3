2e7d958a07551dd91f9d384dade83c0cea13a194
inWeb3ai <charles@inweb3.com>
2023-09-30 11:23:04 -0400

    local tests completed

--- api/api_endpoints.py
+++ api/api_endpoints.py
@@ -13,20 +13,13 @@ from api.routers.well_known import get_ai_plugin, get_host, well_known
 from chatweb3.tools.snowflake_database.tool_custom import (
     CheckTableSummaryTool,
     CheckTableMetadataTool,
-    #    CheckQuerySyntaxTool,
     QueryDatabaseTool,
 )
 from chatweb3.create_agent import get_snowflake_container
-from config.config import agent_config
 
 
 logger = get_logger(__name__)
 
-# load_dotenv()
-
-# Create the agent
-# CONVERSATION_MODE = agent_config.get("agent.conversational_chat")
-# agent_executor = create_agent_executor(conversation_mode=CONVERSATION_MODE)
 
 db = get_snowflake_container()
 
@@ -75,6 +68,7 @@ async def general_exception_handler(request: Request, exc: Exception):
 @app.get("/get_list_of_available_tables")
 async def get_list_of_available_tables(table_list: str = ""):
     try:
+        logger.debug(f"tool_input={table_list} Fetching list of available tables...")
         tool = CheckTableSummaryTool(db=db)
         result = tool.run(tool_input=table_list)
         logger.debug(
@@ -87,7 +81,7 @@ async def get_list_of_available_tables(table_list: str = ""):
 
 
 # Endpoint: Get Detailed Metadata for Tables
-@app.get("/get_detailed_metadata_for_tables/")
+@app.get("/get_detailed_metadata_for_tables")
 async def get_detailed_metadata_for_tables(table_names: str):
     try:
         tool = CheckTableMetadataTool(db=db)
@@ -123,70 +117,3 @@ def start():
     import uvicorn
 
     uvicorn.run("api.api_endpoints:app", host="localhost", port=8000, reload=True)
-
-
-# class ChatWeb3QueryRequest(BaseModel):
-#     query: str = Field(
-#         ...,
-#         description="A natural English language query to query blockchain data",
-#     )
-
-
-# @app.post("/query_blockchain_data", include_in_schema=True)
-# async def query_chatweb3(data: ChatWeb3QueryRequest, request: Request):
-#     """Query blockchain data using a natural English language query
-#     and get the answer as well as the thought process
-#     """
-#     try:
-#         answer, thought_process = query_blockchain_data_from_flipside(data.query)
-#         return {"answer": answer, "thought_process": thought_process}
-#     except BlockchainDataError as e:
-#         return JSONResponse(status_code=400, content={"error": str(e)})
-#     except Exception as e:
-#         return JSONResponse(
-#             status_code=500, content={"error": "Internal server error" + str(e)}
-#         )
-
-
-# @app.exception_handler(BlockchainDataError)
-# async def unicorn_exception_handler(request: Request, exc: BlockchainDataError):
-#     return JSONResponse(
-#         status_code=400,
-#         content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-#     )
-
-
-# # Endpoint: Check Snowflake SQL Query Syntax
-# @app.post("/check_snowflake_sql_query_syntax")
-# async def check_snowflake_sql_query_syntax(query: SnowflakeQuery):
-#     try:
-#         template = SNOWFLAKE_QUERY_CHECKER
-#         llm = ChatOpenAI(temperature=0)
-
-#         messages = [
-#             SystemMessagePromptTemplate.from_template(template=SNOWFLAKE_QUERY_CHECKER),
-#             HumanMessagePromptTemplate.from_template(template="\n\n{query}"),
-#         ]
-#         input_variables = ["query", "dialect"]
-
-#         # verbose = False if verbose is None else verbose
-
-#         checker_llm_chain = LLMChain(
-#             llm=llm,
-#             prompt=ChatPromptTemplate(
-#                 input_variables=input_variables,
-#                 messages=messages,  # type: ignore[arg-type]
-#             ),
-#             # callback_manager=callback_manager,
-#             callbacks=callbacks,
-#             memory=self.readonly_shared_memory,
-#             verbose=verbose,
-#         )
-
-#         tool = CheckQuerySyntaxTool(db=db)
-#         result = tool.run(query.query)
-#         logger.debug(f"Checked syntax for query: {query.query}.")
-#         return {"result": result}
-#     except Exception as e:
-#         logger.error(f"Error checking syntax for query {query.query}: {e}")
-#         raise HTTPException(status_code=500, detail=str(e))
